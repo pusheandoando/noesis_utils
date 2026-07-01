3b1cd38706a2a26ad91b4e8ae3096d7f9b4836e5
pusheandoando <pusheandoando@gmail.com>
2026-07-01 11:16:11 -0600

    v1.0.1
    feat: verbose prints

--- noesis_utils/router.py
+++ noesis_utils/router.py
@@ -13,5 +13,12 @@ STORAGE_DIR_NAME = ".noesis"
 
 def get_storage_path() -> Path:
     storage_path = Path.home() / STORAGE_DIR_NAME
-    storage_path.mkdir(exist_ok=True)
+
+    if not storage_path.exists():
+        print(f"[noesis_utils] directory '{storage_path}' does not exist, creating it...")
+        storage_path.mkdir(exist_ok=True)
+        print("[noesis_utils] directory created successfully.")
+    else:
+        print(f"[noesis_utils] directory '{storage_path}' already exists.")
+
     return storage_path
